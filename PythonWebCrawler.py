ab77f7fa44b014f940c045fb3c567be0e9aa6170
David.Gong <258779909@qq.com>
2020-03-16 20:51:54 +0800

    Update PythonWebCrawler.py
    优化代码

--- PythonWebCrawler.py
+++ PythonWebCrawler.py
@@ -7,6 +7,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 baseUrl = "https://www.biqukan.com/"
+books = ["16_16578/"]
 title = ""
 def getHTMLText(url):
     try:
@@ -22,8 +23,8 @@ def getContent(url):
     soup=BeautifulSoup(r,'lxml')
     context = soup.find("div",class_="showtxt")
     return context.text
-def main():
-    r = getHTMLText(baseUrl+"45_45900/")
+def getBook(url):
+    r = getHTMLText(baseUrl+url)
     soup=BeautifulSoup(r,'lxml')
     title = soup.title.text
     print(soup.title)
@@ -35,15 +36,21 @@ def main():
     zw = False
     for item in urls:
         if zw:
+            fo = open(fileName, "a")
+            fo.write(item.next.text)
             detail = getContent(item.next.get("href"));
             detail = detail.replace("<br />","\r\n")
+            detail = detail.replace("app2();　　(https://www.biqukan.com/45_45900/21418703.html)chaptererror();","")
+            detail = detail.replace("&1t;/p>","")
             # 打开一个文件
-            fo = open(fileName, "a")
             fo.write(detail)
             fo.close    
             print item.next.get("href")
             print item.next
         if(item.name == "dt" and item.text.count("正文卷")):
             zw = True
+
+def main():
+    getBook(books[0])
 if __name__ == "__main__":
     main()
